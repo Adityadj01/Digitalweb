36bff0e6ab976cb7258a10ae14ab92e537547f16
Aditya Jadhav <jadhavaditya787@gmail.com>
2024-08-05 21:14:22 +0530

    Footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,33 +1,89 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
 const Footer = () => {
   return (
     <div className="dark:bg-gray-950">
       <div className="container">
         <div className="grid md:grid-cols-3 pb-20 pt-5">
           {/* Company Detail */}
-          <div>
+          <div className="py-8 px-4">
             <a
               href="#"
               className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl"
             >
               Eshop
             </a>
-            <p className="text-gray-600 lg:pr-24 pt-3">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum id vero ab recusandae totam.
+            <p className="text-gray-600 dark:text-white/70 lg:pr-24 pt-3">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+              id vero ab recusandae totam.
             </p>
-            <p className="text-gray-500 mt-4">
-                Made With 💖 by MuchaFuuker
-            </p>
+            <p className="text-gray-500 mt-4">Made With 💖 by MuchaFuuker</p>
             <a
-            href="https://github.com/Adityadj01" target="blank" 
+              href="https://github.com/Adityadj01"
+              target="blank"
               className="inline-block bg-primary/90 text-white py-2 px-4 mt-4 text-sm rounded-full hover:scale-105 duration-300 "
             >
-                Visit GitHub
+              Visit GitHub
             </a>
           </div>
           {/* Footer Links */}
+          <div className="col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10">
+            <div className="py-8 px-4">
+              <h1 className="text-xl font-bold sm:text-left mb-3">
+                Important links
+              </h1>
+              <ul className="space-y-3">
+                {FooterLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="text-gray-600 dark:text-gray-400 hover:text-black hover:dark:text-white duration-300"
+                    >
+                      {data.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* Second Col links */}
+            <div className="py-8 px-4">
+              <h1 className="text-xl font-bold sm:text-left mb-3">
+                Quick links
+              </h1>
+              <ul className="space-y-3">
+                {FooterLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="text-gray-600 dark:text-gray-400 hover:text-black hover:dark:text-white duration-300"
+                    >
+                      {data.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
